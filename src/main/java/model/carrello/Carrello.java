391cbe252652d9096ea4e6a24537b7920b715441
SimoneC01-commit <s.caruso20@studenti.unisa.it>
2026-07-22 17:44:26 +0200

    lil fix sul carrello

--- src/main/java/model/carrello/Carrello.java
+++ src/main/java/model/carrello/Carrello.java
@@ -7,13 +7,19 @@ import model.prodotto.ProdottoBean;
 
 public class Carrello {
 	private ArrayList<ProdottoBean> lista;
+	private int numElem;
 
 	public Carrello() {
 		lista = new ArrayList<ProdottoBean>();
+		numElem = 0;
 	}
 	
 	public ArrayList<ProdottoBean> getLista(){
 		return lista;
+	}
+	
+	public int getNumElem(){
+		return numElem;
 	}
 	
 	public void aggiungiProdotto(ProdottoBean prodotto) {
@@ -23,14 +29,17 @@ public class Carrello {
 	        }
 	    }
 	    this.lista.add(prodotto);
+	    this.numElem++;
 	}
 	
 	public void rimuoviProdotto(int idProdotto) {
         lista.removeIf(p -> p.getIdProdotto() == idProdotto);
+        numElem--;
     }
 	
 	public void svuota() {
         lista.clear();
+        numElem = 0;
     }
 	
 	public BigDecimal getTotale() {
